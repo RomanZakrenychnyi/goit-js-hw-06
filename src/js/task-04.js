0c9834b118ebbb39bcda161b264e8e0d222c250a
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-01-10 23:34:38 +0200

    task-4,5 done

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,29 +1,18 @@
 // 1. Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
+// 2. Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
+// 3. Обновляй интерфейс новым значением переменной counterValue.
 let counterValue = 0
 
-// 2. Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
+const decrement = document.querySelector('button[data-action="decrement"]')
+const increment = document.querySelector('button[data-action="increment"]')
+const counter = document.querySelector('#value')
 
-// 3. Обновляй интерфейс новым значением переменной counterValue.
+decrement.addEventListener('click', () => {
+    counterValue -= 1
+    counter.textContent = counterValue
+})
 
-
-
-
-
-
-
-
-
-
-// const displayCounter = document.querySelector('#value')
-// const buttonMinus = document.querySelector('button[data-action="decrement"]')
-// const buttonPlus = document.querySelector('button[data-action="increment"]')
-
-// buttonMinus.addEventListener('click', () => {
-// 	counterValue--
-// 	displayCounter.textContent = counterValue
-// })
-
-// buttonPlus.addEventListener('click', () => {
-// 	counterValue++
-// 	displayCounter.textContent = counterValue
-// })
+increment.addEventListener('click', () => {
+    counterValue += 1
+    counter.textContent = counterValue
+})
--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,10 +1,14 @@
-// const textInput = document.querySelector('#name-input')
-// const output = document.querySelector('#name-output')
+// 1. Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), 
+// подставляет его текущее значение в span#name - output.
+// 2. Если инпут пустой, в спане должна отображаться строка "Anonymous".
 
-// textInput.addEventListener('input', event => {
-// 	output.textContent = event.target.value
+const input = document.querySelector('#name-input')
+const output = document.querySelector('#name-output')
 
-// 	if (event.target.value === '') {
-// 		output.textContent = 'Anonymous'
-// 	}
-// })
+input.addEventListener('input', event => {
+    output.textContent = event.target.value
+
+    if (event.target.value === '') {
+        output.textContent = 'Anonymous'
+    }
+})
