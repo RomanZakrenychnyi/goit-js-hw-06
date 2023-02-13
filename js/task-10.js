a223035b7b6a105a88fca2b35de0070b1724b48b
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-02-13 17:45:50 +0200

    task 10 done!!!

--- js/task-10.js
+++ js/task-10.js
@@ -1,27 +1,39 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
 const inputEl = document.querySelector('input');
 const btnCreatEl = document.querySelector('[data-action="create"]');
 const btnDestroyEl = document.querySelector('[data-action="destroy"]');
 const boxColectionEl = document.querySelector('#boxes');
 
+boxColectionEl.style.cssText = `display: flex; flex-wrap: wrap; gap: 10px`
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 const creatNumberOfBoxes = () => {
-  const amount = +(inputEl.value)
-console.log(amount);
+   return +(inputEl.value)
 }
 
 const creatBoxEl = btnCreatEl.addEventListener('click', event => {
-  const randomColor = getRandomHexColor();
+  boxColectionEl.innerHTML = '';
+  const inputValueEl = creatNumberOfBoxes();
+  let boxSize = 20;
   const box = document.createElement('div');
 
-  box.style.cssText = `width: 30px; height: 30px; background: ${randomColor}; margin-top: 10px`
-  boxColectionEl.style.cssText = `display: flex; flex-wrap: wrap; gap: 10px`
-  boxColectionEl.append(box)
+  for (let i = 0; i < inputValueEl; i += 1) {
+    const randomColor = getRandomHexColor();
+    boxSize += 10;
+    if (inputValueEl === 0) {
+      return;
+    }
+    
+    box.style.cssText = `width: ${boxSize}px; height: ${boxSize}px; background: ${randomColor}; margin-top: 10px`
+    console.log(box);
+  }
 })
 
+boxColectionEl.append(box)
 
 btnDestroyEl.addEventListener('click', () => {
   boxColectionEl.innerHTML = '';
+  inputEl.value = '';
 })
