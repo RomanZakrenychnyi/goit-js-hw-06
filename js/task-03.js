2542b293596b2d141d80c0c1c13fd6fb90cb3352
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-02-27 11:17:02 +0200

    task 3, add images refs

--- js/task-03.js
+++ js/task-03.js
@@ -1,5 +1,20 @@
 const listEl = document.querySelector('.gallery')
 
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
 const creatListItemEls = images.map(({ url, alt }) => `
   <li class = ''>
   <img src = ${url} alt = ${alt} width = 480px>
